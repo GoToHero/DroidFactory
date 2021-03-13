a067c96cb5860632ae7c49f90e2597a461e9c92c
saper663_home <saper663ru@gmail.com>
2021-03-13 15:54:07 +0500

    add MemesScreenFragmentViewModel

---
+++ app/src/main/java/ru/androidacademy/droidfactory/features/memesScreen/MemesScreenFragmentViewModel.kt
@@ -0,0 +1,10 @@
+package ru.androidacademy.droidfactory.features.memesScreen
+
+import androidx.lifecycle.ViewModel
+
+class MemesScreenFragmentViewModel : ViewModel() {
+
+    // private var _memes = MutableLiveData<List<Memes>>()
+    // val memes: LiveData<List<Memes>> get() = _memes
+
+}
