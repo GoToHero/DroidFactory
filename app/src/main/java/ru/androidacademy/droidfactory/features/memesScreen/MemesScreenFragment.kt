b629a7fb99cb1690b5149cbd7f11d543c4e32182
saper663_home <saper663ru@gmail.com>
2021-03-14 00:01:57 +0500

    add resources

--- app/src/main/java/ru/androidacademy/droidfactory/features/memesScreen/MemesScreenFragment.kt
+++ app/src/main/java/ru/androidacademy/droidfactory/features/memesScreen/MemesScreenFragment.kt
@@ -2,8 +2,10 @@ package ru.androidacademy.droidfactory.features.memesScreen
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import ru.androidacademy.droidfactory.R
 import ru.androidacademy.droidfactory.databinding.MemesScreenFragmentBinding
 
@@ -13,21 +15,33 @@ class MemesScreenFragment : Fragment(R.layout.memes_screen_fragment) {
     private val binding get() = _binding!!
     private lateinit var viewModel: MemesScreenFragmentViewModel
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = MemesScreenFragmentBinding.bind(view)
 
         val viewModelFactory = MemesScreenFragmentViewModelFactory()
-        viewModel =
-            ViewModelProvider(this, viewModelFactory).get(MemesScreenFragmentViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(MemesScreenFragmentViewModel::class.java)
+        viewModel.loadingState.observe(this.viewLifecycleOwner, this::setLoading)
+        initErrorHandler()
 
     }
-
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
 
+    private fun setLoading(loading: Boolean) {
+        binding.progressBar.isVisible = loading
+    }
+
+    private fun initErrorHandler() {
+        viewModel.error.observe(viewLifecycleOwner, { errorMessage ->
+            Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_SHORT)
+                .show()
+        })
+    }
 }
--- app/src/main/java/ru/androidacademy/droidfactory/features/memesScreen/MemesScreenFragmentViewModel.kt
+++ app/src/main/java/ru/androidacademy/droidfactory/features/memesScreen/MemesScreenFragmentViewModel.kt
@@ -1,10 +1,34 @@
 package ru.androidacademy.droidfactory.features.memesScreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.androidacademy.droidfactory.MemsData
+import ru.androidacademy.droidfactory.Repository
+import ru.androidacademy.droidfactory.network.MemsResources
 
 class MemesScreenFragmentViewModel : ViewModel() {
 
-    // private var _memes = MutableLiveData<List<Memes>>()
-    // val memes: LiveData<List<Memes>> get() = _memes
+     private var _memes = MutableLiveData<MemsResources<List<MemsData>>>()
+     val memes: LiveData<MemsResources<List<MemsData>>> get() = _memes
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    private val _mutableLoadingState = MutableLiveData(false)
+    val loadingState: LiveData<Boolean> get() = _mutableLoadingState
+
+    init {
+        viewModelScope.launch {
+            try {
+                _memes.value = Repository.initialize()
+            } catch (error: Throwable) {
+                _error.value = "Error loaded"
+            }
+
+        }
+    }
 
 }
